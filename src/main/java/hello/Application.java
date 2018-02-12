f2a316a9bf1ec51d0c8aaaf5ed21646c26e4d308
ckelner <chris.kelner@gmail.com>
2018-02-12 17:43:17 -0500

    add some log statements

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,18 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @RestController
 public class Application {
+    // kelnerhax - throw some output in logs to test tracing (know it loaded)
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("/")
     public String home() {
+        logger.info("Home loaded!");
         return "Hello Docker World";
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
