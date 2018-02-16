e40784520a4bbe957824830ae20a7d7b5d10f885
ckelner <chris.kelner@gmail.com>
2018-02-16 16:40:29 -0500

    Clojure helloworld project

---
+++ clojure/src/helloworld/core.clj
@@ -0,0 +1,10 @@
+(ns helloworld.core
+  (:require [ring.adapter.jetty :as jetty]))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello Clojure, Hello Ring!"})
+
+(defn -main []
+  (jetty/run-jetty handler {:port 8080}))
